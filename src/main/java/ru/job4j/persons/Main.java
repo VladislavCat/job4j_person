d768a98bf9a2f8539571cb5e01dc8c2665bc8b12
VladislavCat <kshift84@gmail.com>
2023-02-21 20:05:58 +0300

    Создание модели и main

---
+++ src/main/java/ru/job4j/persons/Main.java
@@ -0,0 +1,15 @@
+package ru.job4j.persons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import ru.job4j.persons.repository.PersonRepository;
+
+@SpringBootApplication
+@ComponentScan("ru.job4j.persons.repository")
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/ru/job4j/persons/model/Person.java
@@ -0,0 +1,34 @@
+package ru.job4j.persons.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Data
+@Table(name = "person")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
