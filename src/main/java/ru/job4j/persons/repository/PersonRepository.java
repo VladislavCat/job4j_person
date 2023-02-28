2b6164805a71e793c56e661b1c360a394f3ee9e6
VladislavCat <kshift84@gmail.com>
2023-02-28 13:57:40 +0300

    Авторизация в job4j_persons с JWT

--- src/main/java/ru/job4j/persons/repository/PersonRepository.java
+++ src/main/java/ru/job4j/persons/repository/PersonRepository.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Repository;
 import ru.job4j.persons.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
     List<Person> findAll();
+
+    Person findByLogin(String name);
 }
